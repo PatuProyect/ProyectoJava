5513107c1c3d5cc3854be908e9c5844a7fc6f317
Diego Bastida <diegoabastida@gmail.com>
2022-06-16 11:31:05 -0300

    Agregando Dom

--- scripts/index.js
+++ scripts/index.js
@@ -1,104 +1,56 @@
-alert("Conozca cuanto sale un terreno en Zona Oeste")
+
 let nombre = prompt("Ingrese su nombre")
-while (nombre == "") {
-    nombre= prompt("Ingrese su nombre");
-}
-let elegirTerreno = prompt("Elija el tipo de su terreno \n1 - Terreno personalizado \n2 - Terreno predefinido")
 
-if(elegirTerreno == 1){
-metrosDelTerreno();
-}
+let contenedorTitulo = document.getElementById("contenedorTitulo")
+let titulo = document.createElement("h1")
+titulo.innerHTML = "Bienvenido " + nombre.toUpperCase() + " a la venta de terrenos en Argentina"
+contenedorTitulo.append(titulo)
 
-function metrosDelTerreno (){
-    let metrosAncho = prompt("Ingresa el ancho del terreno deseado");
-    while (metrosAncho == "") {
-        metrosAncho= prompt("Ingresa el ancho del terreno deseado");
-        }
-    let metrosLargo = prompt("Ingresa el largo del terreno deseado");
-    while (metrosLargo == "") {
-        metrosLargo= prompt("Ingresa el largo del terreno deseado");
-        }
-    metrosTotal = (metrosLargo * metrosAncho)
-    alert("Hola " + nombre + ", la cantidad de metros cuadrados de su terreno es: " + metrosTotal +" m2" + "\nEn Breve le informamos el costo del mismo.")
-    agregarCostos();
-    elegirTerreno = prompt("Desea consultar otro terreno ? \n1 - Terreno personalizado \n2 - Terreno predefinido \n3 - no")
-    switch (elegirTerreno) {
-        case "1":
-            metrosDelTerreno();
-        case "2":
-            eleccionTerreno();
-        default:
-            break;
-    }
-    return elegirTerreno
-}
+const tiposTerrenos = [{
+    id: "1",
+    category: "terrenos",
+    details: "Ancho 10mts \nAlto 20 mts \nM2 Total: 200",
+    name: "Terreno Chico",
+    price: 4000,
+    img: "./assets/imagenes/productos/terrenochico.jpg",
+},{
+    id: "2",
+    category: "terrenos",
+    details: "Ancho 12mts \nAlto 25 mts \nM2 Total: 300",
+    name: "Terreno Mediano",
+    price: 6000,
+    img: "./assets/imagenes/productos/terrenomediano.jpg"
+},{
+    id: "3",
+    category: "terrenos",
+    details: "Ancho 20 mts \nAlto 40 mts \nM2 Total: 800",
+    name: "Terreno Grande",
+    price: 9000,
+    img: "./assets/imagenes/productos/terrenogrande.jpg"
+}]
 
-function agregarCostos(){
-    let precioMetros = 1000;
-    let costoTerreno = precioMetros * metrosTotal;
-    let impuestos = 15;
-    let costoTotal = costoTerreno + ((impuestos/100)*costoTerreno);
-    alert ("El precio para " +metrosTotal+" m2 es: u$d " + costoTotal);
-}
- */
-class terrenoPredefinido {
-    constructor (nombre, mAncho,mLargo,mTotal,precio){
-        this.nombre = nombre
-        this.mAncho = mAncho
-        this.mLargo = mLargo
-        this.mTotal = mTotal
-        this.precio = precio
-    }
+function mostrarProductos() {
+    tiposTerrenos.forEach((product) => {
+        let card = document.createElement("div")
+        card.setAttribute("class", "boxProducto")
+        conteiner.append(card)
+        let img = document.createElement("img")
+        img.setAttribute("src", product.img)
+        img.setAttribute("class","imagenProducto")
+        let name = document.createElement("h3")
+        name.innerText = (product.name)
+        let price = document.createElement("p")
+        price.innerText = (product.price)
+        let details = document.createElement("p")
+        details.innerText = (product.details)
+        let buyButton = document.createElement("button")
+        buyButton.innerText = ("Conosca mas sobre este terreno")
+        card.append(img, name, price, details, buyButton)
+    })
 }
 
-const terrenoChico = new terrenoPredefinido("terreno chico",10,20,200,"$ 4.000")
-const terrenoMediano = new terrenoPredefinido("terreno mediano",12,25,300,"$ 6.000")
-const terrenoGrande = new terrenoPredefinido("terreno grande",20,40,800,"$ 15.000")
+mostrarProductos()
 
-
-function eleccionTerreno(){
-    let terrenoElegido = prompt("Elija el que terreno que desea: \n1 - Terreno chico \n2 - Terreno mediano \n3 - Terreno grande")
-    while(terrenoElegido > 3 || isNaN(terrenoElegido)){
-        alert("Eligio una opcion no valida")
-        terrenoElegido = prompt("Por favor elija su tipo de terreno: \n1 - Terreno chico \n2 - Terreno mediano \n3 - Terreno grande")
-    }
-    return terrenoElegido
-}
-
-switch (eleccionTerreno()) {
-    case "1":
-        console.log(terrenoChico)
-        break;
-    case "2":
-        console.log(terrenoMediano)
-        break;
-    case "3":
-        console.log(terrenoGrande)
-        break;
-    default:
-        break;
-}
-
-const array = []
-
-array.push(terrenoChico)
-array.push(terrenoGrande)
-array.push(terrenoMediano)
-
-let terrenosPequeños = array.filter((index)=>{
-    return index.mTotal <= "250"
-})
-
-console.log(terrenosPequeños)
-
-
-
-
-
-
-
-
-
-
+console.log(tiposTerrenos)
 
 
